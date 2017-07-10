194cfc308fb313ee35e2791210c63d2bbbcac732
elwin_mao <elwin_mao@163.com>
2017-07-10 21:45:36 +0800

    chang user space code

--- test_word_count.c
+++ test_word_count.c
@@ -11,7 +11,6 @@ static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
 int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
-   printf("Starting device test code example...\n");
    fd = open("/dev/wordcount", O_RDWR);             // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
@@ -19,26 +18,20 @@ int main(){
    }
    while(1)
    {
-   printf("Type in a short string to send to the kernel module:\n");
-   scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
-   printf("Writing message to the device [%s].\n", stringToSend);
+   scanf("%s", stringToSend);                // Read in a string (with spaces)
    ret = write(fd, stringToSend, strlen(stringToSend)+1); // Send the string to the LKM
    if (ret < 0){
       perror("Failed to write the message to the device.");
       return errno;
    }
 
-   printf("Press ENTER to read back from the device...\n");
-   getchar();
-
-   printf("Reading from the device...\n");
    ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
    if (ret < 0){
       perror("Failed to read the message from the device.");
       return errno;
    }
    receive[ret+1]='\0';
-   printf("The received message is: [%s]\n", receive);
+   printf("The received message is: %s", receive);
    }
    printf("End of the program\n");
    return 0;
